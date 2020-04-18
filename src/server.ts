35c16b10e58505e03786e11b4a27dabc0e619de4
Antonio Sampaio De Vuono <antoniosvuono@gmail.com>
2020-04-18 17:16:25 -0300

    Importando arquivos typescript

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({ message: 'Hello GoBarber! 🤓'});
-})
+app.use(routes);
+
 
 app.listen(3333, () => {
-    console.log('🚀 Server started on port 3333!');
+  console.log('🚀 Server started on port 3333!');
 });
